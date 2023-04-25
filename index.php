54bd1fd350781f294718f894bf3d74b26368090c
santtiagocristancho <deibysantiagocrist@gmail.com>
2023-04-24 22:34:55 -0500

    prueba_9

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once 'views/main/partials/header.php';
 ?>
 
 <!-- Content -->
-<h1>Bienvenide kevin</h1>
+<h1>Bienvenide deiby santiago cristancho</h1>
 
 <?php
 include_once 'views/main/partials/footer.php';
