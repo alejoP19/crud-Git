fb57b10e0f951b978193b032b3ab205d6f828083
santtiagocristancho <deibysantiagocrist@gmail.com>
2023-04-24 22:43:31 -0500

    iniciando commit

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 include_once 'views/main/partials/header.php';
 ?>
 
-<!-- Content -->
-<h1>Bienvenide deiby jimenz</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>hello world</h2>
+</body>
+</html>
 
 <?php
 include_once 'views/main/partials/footer.php';
