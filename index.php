914c437e985664ff8931b305b207993b0000cd27
santtiagocristancho <deibysantiagocrist@gmail.com>
2023-04-24 22:45:22 -0500

    add texts

--- index.php
+++ index.php
@@ -12,6 +12,11 @@ include_once 'views/main/partials/header.php';
 </head>
 <body>
     <h2>hello world</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero est quia modi, quos aliquam perferendis sunt totam cupiditate veritatis quaerat voluptas maiores dolore earum facilis officiis autem nisi impedit optio!
+    <p>de nuevo</p>
+    <div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis exercitationem assumenda sed quisquam maxime dolorum iure totam saepe, optio sint voluptas iusto modi qui reprehenderit libero veritatis repellat aliquam voluptatum.</p>
+    </div>
 </body>
 </html>
 
